f1c81416d8d56c84b2551abcea7f29b614af6e30
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2025-12-04 18:57:22 -0300

    add: LOgin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,76 @@
 import Layout from "../components/Layout";
 import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+
   const { login } = useAuth()
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`http://localhost:2222/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      const responseData = await response.json()
+      if (responseData.error) {
+        alert(responseData.error)
+      }
+
+      console.log(responseData)
+
+      login(responseData.data.token)
+
+
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <Layout>
-      <h1>Login</h1>
+      <div className="auth-container">
+        <h1>Iniciar sesión</h1>
+        <form className="auth-form" onSubmit={handleSubmit}>
+          <input
+            className="auth-input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            required
+          />
+          <input
+            className="auth-input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+          <button className=" btn btn-primary" type="submit">Iniciar sesión</button>
+        </form>
+      </div>
     </Layout>
   );
 };

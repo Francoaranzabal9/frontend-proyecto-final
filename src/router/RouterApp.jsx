bddb893c1f5ad0db0645987cb32492da2021cf94
francoaranzabal9 <juanfrancoaranzabal@gmail.com>
2026-01-31 11:59:41 -0300

    feat: update application routing

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -9,6 +9,11 @@ import Cart from "../pages/Cart";
 import { PrivateRoute } from "../components/PrivateRoute";
 import { GetPerfume } from "../pages/GetPerfume";
 import NotFound from "../pages/NotFound";
+import Checkout from "../pages/Checkout";
+import Success from "../pages/Success";
+import Dashboard from "../pages/Dashboard";
+import OrderDetail from "../pages/OrderDetail";
+import AdminRoute from "../components/AdminRoute";
 
 const RouterApp = () => {
   return (
@@ -19,14 +24,32 @@ const RouterApp = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} />
         <Route path="/agregar-producto" element={
-          <PrivateRoute>
+          <AdminRoute>
             <AddProduct />
-          </PrivateRoute>
+          </AdminRoute>
         }
         />
         <Route path="/get-perfume/:id" element={<GetPerfume />} />
         <Route path="/contacto" element={<Contact />} />
         <Route path="/carrito" element={<Cart />} />
+        <Route path="/checkout" element={
+          <PrivateRoute>
+            <Checkout />
+          </PrivateRoute>
+        } />
+        <Route path="/success" element={<Success />} />
+        <Route path="/failure" element={<Success />} />
+        <Route path="/pending" element={<Success />} />
+        <Route path="/dashboard" element={
+          <AdminRoute>
+            <Dashboard />
+          </AdminRoute>
+        } />
+        <Route path="/orders/:id" element={
+          <AdminRoute>
+            <OrderDetail />
+          </AdminRoute>
+        } />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
